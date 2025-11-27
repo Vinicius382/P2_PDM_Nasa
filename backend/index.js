f9c89fd23cd98dc1876ed2334da0bad8055a5520
Vinicius <oliveiravinicius010@gmail.com>
2025-11-27 13:21:18 -0300

    feat(p2): conecta fotos do dia com api

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 require('dotenv').config()
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 const port = 3000
+const NASA_API_KEY = process.env.NASA_API_KEY
 
-app.listen(port, () => { console.log("Servidor na Porta ${port}") 
+app.get('/imagem-dia', async (req, res) => {
+    const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`)
+    res.json(response.data)
+})
+
+app.get('/buscar', async (req, res) => {
+    const { termo, ano } = req.query
+    const response = await axios.get(`https://images-api.nasa.gov/search?q=${termo}&year_start=${ano}&year_end=${ano}&media_type=image`)
+
+app.listen(port, () => { console.log(`Servidor na Porta ${port}`) 
 })
